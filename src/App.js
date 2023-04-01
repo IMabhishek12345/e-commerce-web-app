8cddf5d861ed9b4574be157f51fca478456370db
abhishek pokhriyal <abhishek.pokhriyal8@gmail.com>
2023-04-01 14:07:57 +0530

    creating a checout page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
      //BEM convention 
+    <Router>
     <div className="app">
-    <Header/>
-    <Home/>
-     
-     
+     <Routes>
+      <Route exact path="/" element={[<Header/>,<Home/>]}/>
+      <Route exact path="/checkout" element={[<Header/>,<h1>Hi i am the checkout page</h1>]}/>
+     </Routes>
 
-    </div>
+     </div>
+   </Router>
   );
 }
 
