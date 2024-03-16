0d1c23742e2b1e6ad86e2aa20eaf689715cffe52
Abhishek Pokhriyal <abhishek.pokhriyal8@gmail.com>
2024-03-16 11:31:52 +0530

    created some changes in bootstrap code

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Home.css";
 import Product from './Product';
-
+  
 const Home = () => {
     return (
         <div className='home'>
@@ -12,31 +12,32 @@ const Home = () => {
                 <img  src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Kartik/MARCHGTM/IPL2023/1percentCB/Recharge_PC_Hero_3000x1200_1._CB593370740_.jpg" className="d-block w-100" alt="..."/> */}
         
          {/* <img className="home__image" src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/815-245OLFL._SX3000_.jpg" alt="" /> */}
-         <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active" data-bs-interval="2000">
-                <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg"/>
-                </div>
-                <div className="carousel-item" data-bs-interval="2000">
-                <img src="https://m.media-amazon.com/images/I/61+Om+g+8SL._SX3000_.jpg" className="home__image" alt="..."/>
-                </div>
-                <div className="carousel-item" data-bs-interval="2000">
-                <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Kartik/MARCHGTM/IPL2023/1percentCB/Recharge_PC_Hero_3000x1200_1._CB593370740_.jpg" className="home__image" alt="..."/>
-                </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </div>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg" class="d-block w-100" alt="..."/>
+      </div>
+      <div class="carousel-item">
+        <img src="https://m.media-amazon.com/images/I/61+Om+g+8SL._SX3000_.jpg" class="d-block w-100" alt="..."/>
+      </div>
+      <div class="carousel-item">
+        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Kartik/MARCHGTM/IPL2023/1percentCB/Recharge_PC_Hero_3000x1200_1._CB593370740_.jpg" className="home__image" class="d-block w-100" alt="..."/>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+     </button>
+   </div>    
+
 
         <div className="home__row">
-            <Product id={12345} title="Home appliances" image={"https://images-eu.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B07G5J5FYP._SY116_CB667322346_.jpg"} price={4999} />
-            <Product id={12346} title="Laptop"  image={"https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/G/31/img19/AMS/Houseads/Laptops-Sept2019._CB436595915_.jpg"} price={45000} />
+            <Product id={12345} title="Home appliances" image={"https://m.media-amazon.com/images/I/61qWeUnn9TL._AC_UL320_.jpg"} price={4999} />
+            <Product id={12346} title="Laptop"  image={"https://m.media-amazon.com/images/I/616gUvx+8NL._AC_UL320_.jpg"} price={45000} />
         </div>
             
         <div className="home__row">
@@ -45,7 +46,7 @@ const Home = () => {
             <Product id={12349} title="Trolly and backpacks" image={"https://m.media-amazon.com/images/I/41wN8UcVDuL._AC_UF226,226_FMjpg_.jpg"} price={9999} />          
         </div>
         <div className="home__row">
-            <Product id={12350} title="Smart televison " image={"https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71L-lTQnJiL._AC_UY218_.jpg"} price={4999} />
+            <Product id={12350} title="Smart televison " image={"https://m.media-amazon.com/images/I/81s35LoX36L._AC_UY218_.jpg"} price={4999} />
         </div>
     
        </div>
